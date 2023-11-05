57192b6b0cdffacd763a1047473bb78d57003e05
Jongeun <bubble2948@naver.com>
2023-11-05 17:29:46 +0900

    Compose Activities

---
+++ app/src/main/java/com/example/bbteamgo/CustomerJoinActivity.java
@@ -0,0 +1,14 @@
+package com.example.bbteamgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerJoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_join);
+    }
+}
