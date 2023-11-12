6c9b5971812e9c28eea84f2c32ffdcc94abe4b55
Jongeun <bubble2948@naver.com>
2023-11-12 11:46:44 +0900

    Booth Selection Screen and Navigation Bar

---
+++ app/src/main/java/com/example/bbteamgo/ManagerStartActivity.java
@@ -0,0 +1,14 @@
+package com.example.bbteamgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_start);
+    }
+}
