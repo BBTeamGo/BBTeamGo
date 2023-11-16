b538e704a0f7fa7dc7e4839933a2eb88f482186c
GongMinGi <komki@naver.com>
2023-11-16 20:47:39 +0900

    위와동일

--- app/src/main/java/com/example/bbteamgo/BookingManagementAdapter.java
+++ app/src/main/java/com/example/bbteamgo/BookingManagementAdapter.java
@@ -1,2 +1,48 @@
-package com.example.bbteamgo;public class BookingManagementAdapter {
+package com.example.bbteamgo;
+
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+
+public class BookingManagementAdapter extends RecyclerView.Adapter<BookingManagmentViewHolder>{
+
+    Context context;
+    List<BookingManagementData> data;
+
+    public BookingManagementAdapter(Context context, List<BookingManagementData> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public BookingManagmentViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new BookingManagmentViewHolder(LayoutInflater.from(context).inflate(R.layout.booking_management_item_view, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BookingManagmentViewHolder holder, int position) {
+        holder.bookingOrder.setText(String.valueOf(data.get(position).getBookingOrder()));
+        holder.bookerName.setText(data.get(position).getBookerName());
+        holder.elapsedTime.setText(data.get(position).getElapsedTime());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
 }
+
+
+//booking management page activity의 리사이클러뷰에 사용되는 어댑터.
