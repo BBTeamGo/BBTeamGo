75f3b7e5622d2ee275e4d209be48547b9b321f22
Jongeun <bubble2948@naver.com>
2023-11-26 21:58:22 +0900

    Manager Activity with Navigation Bar

--- app/src/main/java/com/example/bbteamgo/ManagerBoothPasswordDialogActivity.java
+++ app/src/main/java/com/example/bbteamgo/ManagerBoothPasswordDialogActivity.java
@@ -9,6 +9,6 @@ public class ManagerBoothPasswordDialogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manager_booth_password_dialog);
+        setContentView(R.layout.dialog_manager_booth_password);
     }
 }
