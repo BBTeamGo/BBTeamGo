68e3960065713055c134f52d1a842565f39519a2
Jongeun <bubble2948@naver.com>
2023-11-29 23:08:40 +0900

    Manager Fragment

--- app/src/main/java/com/example/bbteamgo/ManagerActivity.java
+++ app/src/main/java/com/example/bbteamgo/ManagerActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ManagerTableActivity extends AppCompatActivity {
+public class ManagerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manager_table);
+        setContentView(R.layout.activity_manager);
     }
 }
