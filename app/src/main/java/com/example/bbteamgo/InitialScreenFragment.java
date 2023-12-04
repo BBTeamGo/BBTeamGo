1bd6fc7743f0062ad3c4285f4f7b0d113ade5156
Jongeun <bubble2948@naver.com>
2023-12-04 20:14:42 +0900

    Main Activity Login

--- app/src/main/java/com/example/bbteamgo/InitialScreenFragment.java
+++ app/src/main/java/com/example/bbteamgo/InitialScreenFragment.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginChoiceActivity extends AppCompatActivity {
+public class InitialScreenFragment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_choice);
+        setContentView(R.layout.fragment_initial_screen);
     }
 }
