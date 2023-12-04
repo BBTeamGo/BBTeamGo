a277c9adfef4710e17a6307d1fde4e6b24fc0244
minhyuk2 <71816821+minhyuk2@users.noreply.github.com>
2023-12-04 22:09:51 +0900

    주문하기 부분

--- app/src/main/java/com/example/bbteamgo/MyReservationActivity.java
+++ app/src/main/java/com/example/bbteamgo/MyReservationActivity.java
@@ -3,12 +3,29 @@ package com.example.bbteamgo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.bbteamgo.databinding.ActivityMyReservationBinding;
 
 public class MyReservationActivity extends AppCompatActivity {
-
+    ActivityMyReservationBinding binding;
+    Button moreReservationBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_reservation);
+        binding = ActivityMyReservationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        moreReservationBtn = findViewById(R.id.moreReservation);
+        moreReservationBtn.setOnClickListener(view->{
+
+           //여기에 item 추가하게끔 만들어주면 된다.
+
+        }
+        );
+
+
+
+
     }
 }
