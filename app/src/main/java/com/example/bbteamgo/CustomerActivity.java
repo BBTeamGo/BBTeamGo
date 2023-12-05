bacdba819a28cc08c13e9854a93cba631f1f83e7
minhyuk2 <71816821+minhyuk2@users.noreply.github.com>
2023-12-05 15:10:17 +0900

    profile 쪽과 customerActivity 쪽을 전부 구현했음

--- app/src/main/java/com/example/bbteamgo/CustomerActivity.java
+++ app/src/main/java/com/example/bbteamgo/CustomerActivity.java
@@ -1,24 +1,73 @@
 package com.example.bbteamgo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.bbteamgo.databinding.ActivityCustomerBinding;
+import com.google.android.material.navigation.NavigationBarItemView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
 
-public class CustomerActivity extends AppCompatActivity {
+    public class CustomerActivity extends AppCompatActivity {
+        private ActivityCustomerBinding binding;
+        CusotmerHomeFragment cusotmerHomeFragment;
+        CusotmerReservationFragment cusotmerReservationFragment;
+        CustomerMyprofileFragment customerMyprofileFragment;
+        orderfragment orderfragment;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_customer);
+            binding = ActivityCustomerBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer);
+            cusotmerHomeFragment = new CusotmerHomeFragment();
+            cusotmerReservationFragment = new CusotmerReservationFragment();
+            customerMyprofileFragment = new CustomerMyprofileFragment();
+            orderfragment = new orderfragment();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_tag,cusotmerHomeFragment).commit();
+
+            NavigationBarView navigationBarView = findViewById(R.id.navigation_bar);
+
+            navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    int itemId = item.getItemId();
+
+                    if( itemId == R.id.home_btn ) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_tag, cusotmerHomeFragment).commit();
+                        return true;
+                    }
+                    else if(itemId == R.id.reservation_button ){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_tag, cusotmerReservationFragment).commit();
+                        return true;
+                    }
+                    else if(itemId== R.id.my_order_button){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_tag,orderfragment).commit();
+                        return true;
+                    }
+                    else if( itemId == R.id.mypage){
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container_view_tag,customerMyprofileFragment).commit();
+                        return true;
+                    }
+
+
+                    return false;
+                }
+            });
+
+
+        }
+
     }
-
-}
--- app/src/main/java/com/example/bbteamgo/CustomerMyprofileFragment.java
+++ app/src/main/java/com/example/bbteamgo/CustomerMyprofileFragment.java
@@ -1,18 +1,26 @@
 package com.example.bbteamgo;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CustomerMyprofileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class CustomerMyprofileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -23,6 +31,11 @@ public class CustomerMyprofileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    public TextView inputname, feelingMessage;
+
+    public EditText eText, eText2;
+    public Button sendButton, cancelButton, sendButton2, cancelButton2;
+
 
     public CustomerMyprofileFragment() {
         // Required empty public constructor
@@ -61,4 +74,64 @@ public class CustomerMyprofileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_customer_myprofile, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        inputname = view.findViewById(R.id.namecard);
+        Dialog nameDialog;
+        nameDialog = new Dialog(getActivity());
+        nameDialog.setContentView(R.layout.namedialog);
+        eText = nameDialog.findViewById(R.id.inputName);
+        sendButton = nameDialog.findViewById(R.id.changeButton);
+        cancelButton = nameDialog.findViewById(R.id.cancelButton);
+        inputname.setOnClickListener( view4 ->
+        {
+            sendButton.setOnClickListener(view1 -> {
+                inputname.setText(eText.getText().toString());
+                Log.d("cmh", eText.getText().toString());
+                nameDialog.dismiss();
+            });
+            cancelButton.setOnClickListener(view2 -> {
+                nameDialog.dismiss();
+            });
+            inputname.setText(eText.getText().toString());
+            nameDialog.show();
+
+        });
+        //프로필 사진 변경하는 부분
+        // 이거는 일단 외부 갤러리에서 데이터를 가져와서 사용해야하기 떄문에 좀 미루고
+        //상태메세지를 변경하는 부분
+        feelingMessage = view.findViewById(R.id.feelingMessage);
+        Dialog feelDialog;
+        feelDialog = new Dialog(getActivity());
+        feelDialog.setContentView(R.layout.feelingdialog);
+        eText2 = feelDialog.findViewById(R.id.feelingInput);
+        sendButton2 = feelDialog.findViewById(R.id.changeButton);
+        cancelButton2 = feelDialog.findViewById(R.id.cancelButton);
+        feelingMessage.setOnClickListener(view5 -> {
+            sendButton2.setOnClickListener(view1 -> {
+                feelingMessage.setText(eText2.getText().toString());
+                Log.d("cmh", eText2.getText().toString());
+                feelDialog.dismiss();
+            });
+            cancelButton2.setOnClickListener(view2 -> {
+                feelDialog.dismiss();
+            });
+            feelingMessage.setText(eText2.getText().toString());
+            feelDialog.show();
+        });
+
+        //로그아웃 기능은 버튼 눌렀을 때 그냥 초기화면으로 보내면 되니까 쉽고
+        //비밀번호 변경은 서버와의 연결이 최우선이니까 이게 제일 중요할듯
+
+
+
+
+    }
 }
+
+
