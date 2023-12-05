0eca1ac27c4ccab01137b392f16803ff628ce4b0
minhyuk2 <71816821+minhyuk2@users.noreply.github.com>
2023-12-05 14:18:24 +0900

    customer쪽 일부분 복구 완료

--- app/src/main/java/com/example/bbteamgo/CusotmerHomeFragment.java
+++ app/src/main/java/com/example/bbteamgo/CusotmerHomeFragment.java
@@ -1,24 +1,44 @@
 package com.example.bbteamgo;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.viewmodel.CreationExtras;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link CusotmerHomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CusotmerHomeFragment extends Fragment {
+public class CusotmerHomeFragment extends Fragment implements OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private GoogleMap mMap;
+    private SearchView searchView;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -60,5 +80,65 @@ public class CusotmerHomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cusotmer_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        searchView = searchView.findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+                if(location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder(getActivity());
+
+                        try {
+                            addressList = geocoder.getFromLocationName(location,1);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+                }
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        mapFragment.getMapAsync(this);
+    }
+
+    private FragmentManager getSupportFragmentManager() {
+        return null;
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        MarkerOptions markerOptions = new MarkerOptions();         // 마커 생성
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");                         // 마커 제목
+        markerOptions.snippet("한국의 수도");         // 마커 설명
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));                 // 초기 위치
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));                         // 줌의 정도
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);                           // 지도 유형 설정
+    }
+
+    @NonNull
+    @Override
+    public CreationExtras getDefaultViewModelCreationExtras() {
+        return super.getDefaultViewModelCreationExtras();
     }
 }
--- app/src/main/java/com/example/bbteamgo/ManagerSelectBoothActivity.java
+++ app/src/main/java/com/example/bbteamgo/ManagerSelectBoothActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-import com.example.bbteamgo.databinding.ActivityMyReservationBinding;
+
 
 public class ManagerSelectBoothActivity extends AppCompatActivity {
 
